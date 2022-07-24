c8d7ce45a89433ad1baf74f0c54bf72c9aef71e6
Elias Gabriel <elia.gabrielfn12@gmail.com>
2022-07-24 15:33:17 -0400

    Create project and remove main.storyboard file

---
+++ TodoListApp/TodoListApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListApp
+//
+//  Created by Elias Gabriel on 24/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
